3334771ec9995b5165bb7257e89d2884f46e0bd8
SIMARPREETKAURKANG <skang8442@gmail.com>
2023-12-27 19:34:22 -0500

    Add backend link commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   //baseUrl: 'https://simply-organic-backend.onrender.com/api',
-  baseUrl: 'https://furniture-store-backend.onrender.com/',
+  baseUrl: 'https://furniture-store-backend.onrender.com/api',
 };
